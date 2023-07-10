b3d53590d9aa0ac1b387e935c8e2596e365a59f8
ssisksl77@gmail.com <ssisksl77@gmail.com>
2023-07-10 23:37:19 +0900

    stage 2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, _ = server_socket.accept() # wait for client
+
+    with conn:
+        conn.recv(1024)
+        conn.send(b"+OK\r\n")
 
 
 if __name__ == "__main__":
