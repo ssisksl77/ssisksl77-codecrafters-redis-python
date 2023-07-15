251567638e368c848a864a09e0ea6f7ae033ed19
남영환 <ssisksl77@gmail.com>
2023-07-16 01:56:32 +0900

    stage 3

--- app/main.py
+++ app/main.py
@@ -11,12 +11,13 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept() # wait for client
 
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.send(b"+PONG\r\n")
+    with server_socket:
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
