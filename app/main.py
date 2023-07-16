40d3b8c179dd7c0eb3eea4da60258418be1316d7
남영환 <ssisksl77@gmail.com>
2023-07-16 09:53:41 +0900

    stage - 5

--- app/main.py
+++ app/main.py
@@ -18,17 +18,48 @@ async def main():
     async with server:
         await server.serve_forever()
 
+def parse(input: str):
+    DATA_TYPES = dict(
+        SIMPLE_STRING="+",
+        ERROR="-",
+        INTEGER=":",
+        BULK_STRING="$",
+        ARRAY="*"
+    )
+
+    command, message = None, None
+    if not input or input[0] not in DATA_TYPES.values():
+        return command, message
+
+    # tokens = input.replace("\\r\\n", "\r\n").split("\r\n")
+    tokens = input.split()
+    print("TOKENS: ", tokens)
+
+    # 중요.
+    data_type = DATA_TYPES.get(tokens[0][0])
+    command_length = tokens[1][1:]  # $은 제거함.
+
+    command = tokens[2]
+    if len(tokens) > 4:
+        message = tokens[4]
+    return command, message
+
+
 # this handler needs the while loop to keep opening for requests
 async def handler(reader, writer):
     while True:
+        print("new connection accepted!")
         data = await reader.read(100)
         # checks data stream so server doesn't crash and wait for data finish sending
         if not data:
             break
 
-        print("new connection accepted!")
-        writer.write(b"+PONG\r\n")
+        command, message = parse(bytes(data).decode())
 
+        if not command or not message or command.lower() == "ping":
+            writer.write(bytes("+PONG\r\n", "utf-8"))
+        elif command.lower() == "echo":
+            writer.write(bytes("+" + message, encoding="utf-8"))
 
 if __name__ == "__main__":
     asyncio.run(main())
